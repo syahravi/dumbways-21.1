089121dd790eae99d0fe2b0df3e021d77fc63da4
Ravi <syahravi.id@gmail.com>
2021-01-30 16:38:53 +0700

    tanpa %def

--- 3.py
+++ 3.py
@@ -1,9 +1,7 @@
-def pola(a):
-        for row in range(5):
-            for col in range(5):
+for row in range(5):
+        for col in range(5):
                 if row+col==2 or col-row==2 or row-col ==2 or row+col==6:
                     print("*", end="")
                 else:
                     print(end=" ")
-            print()
-pola(3)
+        print()
